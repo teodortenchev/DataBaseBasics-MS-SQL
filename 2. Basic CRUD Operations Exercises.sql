7598d40929ac02dc8c0d08c4f725ad913fd60e7a
Teodor <teodortenchev@gmail.com>
2019-02-08 19:49:15 +0400

    Update 2. Basic CRUD Operations Exercises.sql

--- 2. Basic CRUD Operations Exercises.sql
+++ 2. Basic CRUD Operations Exercises.sql
@@ -170,6 +170,11 @@ SELECT TOP(30) CountryName, [Population] FROM Countries
 	WHERE ContinentCode = 'EU'
 ORDER BY [Population] DESC, CountryName ASC
 
+
+
+--24.	 *Countries and Currency (Euro / Not Euro)
+--Find all countries along with information about their currency. Display the country name, country code and information about its currency: either "Euro" or "Not Euro". Sort the results by country name alphabetically.
+
 SELECT CountryName, CountryCode, Currency = 
 		(
 		 CASE 
