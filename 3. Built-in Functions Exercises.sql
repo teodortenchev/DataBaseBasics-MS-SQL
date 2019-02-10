481f758a8a0682749e55465026a62d5bed99c003
Teodor <teodortenchev@gmail.com>
2019-02-10 23:25:50 +0400

    Update 3. Built-in Functions Exercises.sql

--- 3. Built-in Functions Exercises.sql
+++ 3. Built-in Functions Exercises.sql
@@ -73,3 +73,15 @@ SELECT * FROM V_EmployeesHiredAfter2000
 SELECT FirstName, LastName
 FROM Employees
 WHERE LEN(LastName) = 5
+
+
+--Problem 10.	
+--Rank Employees by Salary
+--Write a query that ranks all employees using DENSE_RANK. In the DENSE_RANK function, employees need to be partitioned by Salary and ordered by EmployeeID. You need to find only the employees whose Salary is between 10000 and 50000 and order them by Salary in descending order.
+
+SELECT EmployeeID, FirstName, LastName, Salary
+	, DENSE_RANK() OVER
+	(PARTITION BY Salary ORDER BY EmployeeId) AS Rank
+FROM Employees
+	WHERE Salary >= 10000 AND Salary <= 50000
+		ORDER BY Salary DESC
