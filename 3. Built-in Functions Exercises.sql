fad8a261e7a6793e6deb8b8f93abaef9d6e6b5d0
Teodor <teodortenchev@gmail.com>
2019-02-10 23:04:21 +0400

    Update 3. Built-in Functions Exercises.sql

--- 3. Built-in Functions Exercises.sql
+++ 3. Built-in Functions Exercises.sql
@@ -13,3 +13,10 @@ SELECT FirstName, LastName
 		FROM Employees
 WHERE LastName LIKE '%ei%'
 
+--Problem 3.	Find First Names of All Employees
+--Write a SQL query to find the first names of all employees in the departments with ID 3 or 10 and whose hire year is between 1995 and 2005 inclusive.
+
+SELECT FirstName
+	FROM Employees
+WHERE (DepartmentID = 3 OR DepartmentID = 10) AND 
+			DATEPART(YEAR, HireDate) >= 1995 AND DATEPART(YEAR, HireDate) <=2005
