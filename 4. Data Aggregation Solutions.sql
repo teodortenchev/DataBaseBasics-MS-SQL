dee0f52c83cb8f5b2450ff359043803bd13f1013
Teodor <teodortenchev@gmail.com>
2019-02-14 00:30:22 +0400

    Update 4. Data Aggregation Solutions.sql

--- 4. Data Aggregation Solutions.sql
+++ 4. Data Aggregation Solutions.sql
@@ -12,4 +12,64 @@ FROM WizzardDeposits
 GROUP BY DepositGroup
 
 --Problem 4
+SELECT TOP(2) DepositGroup
+FROM WizzardDeposits
+GROUP BY DepositGroup
+ORDER BY AVG(MagicWandSize)
 
+--Problem 5
+SELECT DepositGroup, SUM(DepositAmount) AS TotalSum
+FROM WizzardDeposits
+GROUP BY DepositGroup
+
+--Problem 6
+SELECT DepositGroup, SUM(DepositAmount) AS TotalSum
+FROM WizzardDeposits
+GROUP BY DepositGroup, MagicWandCreator
+HAVING MagicWandCreator = 'Ollivander family'
+
+--Problem 7
+SELECT DepositGroup, SUM(DepositAmount) AS TotalSum
+FROM WizzardDeposits
+GROUP BY DepositGroup, MagicWandCreator
+HAVING MagicWandCreator = 'Ollivander family' AND SUM(DepositAmount) < 150000
+ORDER BY TotalSum DESC
+
+--Problem 8
+SELECT DepositGroup, MagicWandCreator, MIN(DepositCharge) AS MinDepositCharge
+FROM WizzardDeposits
+GROUP BY DepositGroup, MagicWandCreator
+ORDER BY MagicWandCreator, DepositGroup
+
+--Problem 9
+SELECT [AgeGroup] =
+	(
+		CASE
+			WHEN AGE BETWEEN 0 AND 20 THEN '[0-10]'
+			WHEN Age BETWEEN 11 AND 20 THEN '[11-20]'	
+			WHEN Age BETWEEN 21 AND 30 THEN '[21-30]'
+			WHEN Age BETWEEN 31 AND 40 THEN '[31-40]'	
+			WHEN Age BETWEEN 41 AND 50 THEN '[41-50]'		
+			WHEN Age BETWEEN 51 AND 60 THEN '[51-60]'	
+			WHEN Age >= 61 THEN '[61+]'		
+		END	
+	) , [WizardCount] = COUNT(*)
+FROM WizzardDeposits
+GROUP BY (
+		CASE
+			WHEN AGE BETWEEN 0 AND 20 THEN '[0-10]'
+			WHEN Age BETWEEN 11 AND 20 THEN '[11-20]'	
+			WHEN Age BETWEEN 21 AND 30 THEN '[21-30]'
+			WHEN Age BETWEEN 31 AND 40 THEN '[31-40]'	
+			WHEN Age BETWEEN 41 AND 50 THEN '[41-50]'		
+			WHEN Age BETWEEN 51 AND 60 THEN '[51-60]'	
+			WHEN Age >= 61 THEN '[61+]'		
+		END	
+	)
+	
+--Problem 10
+SELECT LEFT(FirstName, 1) 
+FROM WizzardDeposits
+GROUP BY LEFT(FirstName, 1), DepositGroup
+HAVING DepositGroup = 'Troll Chest'
+ORDER BY LEFT(FirstName, 1)
