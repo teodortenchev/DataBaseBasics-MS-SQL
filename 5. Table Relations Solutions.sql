91e7860925a422ec3b79ffddf904367f3deb6052
Teodor <teodortenchev@gmail.com>
2019-02-18 23:30:21 +0400

    Update 5. Table Relations Solutions.sql

--- 5. Table Relations Solutions.sql
+++ 5. Table Relations Solutions.sql
@@ -71,3 +71,18 @@ CREATE TABLE StudentsExams (
 
 ALTER TABLE StudentsExams
   ADD CONSTRAINT PK_StudentsExams PRIMARY KEY (StudentID, ExamID)
+
+--Problem 4
+ 
+CREATE TABLE Teachers(
+	TeacherID INT PRIMARY KEY IDENTITY(101,1),
+	[Name] VARCHAR(15) NOT NULL,
+	ManagerID INT REFERENCES Teachers(TeacherID)
+)
+
+--Problem 9*
+SELECT m.MountainRange, p.PeakName, p.Elevation 
+    FROM Mountains AS m
+  JOIN Peaks AS p ON m.Id = p.MountainId 
+	WHERE m.MountainRange = 'Rila'
+ ORDER BY p.Elevation DESC
