ec3b6dd1427bdf59775bd7f73639c157a0d0031e
Teodor <teodortenchev@gmail.com>
2019-02-18 21:27:44 +0400

    Update 5. Table Relations Solutions.sql

--- 5. Table Relations Solutions.sql
+++ 5. Table Relations Solutions.sql
@@ -1,1 +1,30 @@
 --Problem 1
+
+CREATE TABLE Persons (
+    PersonID INT PRIMARY KEY,
+		FirstName VARCHAR(30) NOT NULL,
+		Salary DECIMAL(15,2),
+		PassportID INT NOT NULL 
+)
+
+CREATE TABLE Passports (
+    PassportID INT PRIMARY KEY,
+		PassportNumber CHAR(8) NOT NULL
+)
+
+ALTER TABLE Persons
+    ADD CONSTRAINT FK_Persons_Passports FOREIGN KEY (PassportID)
+		  REFERENCES Passports(PassportID)
+
+ALTER TABLE Persons
+    ADD UNIQUE(PassportID)
+
+ALTER TABLE Passports
+    ADD UNIQUE(PassportNumber)
+
+INSERT INTO Passports (PassportID, PassportNumber) VALUES
+ (101, 'N34FG21B'), (102, 'K65LO4R7'), (103, 'ZE657QP2')
+
+INSERT INTO Persons VALUES
+ (1, 'Roberto', 43300.00, 102), (2, 'Tom', 56100.00, 103), 
+ (3, 'Yana', 60200.00, 101)
