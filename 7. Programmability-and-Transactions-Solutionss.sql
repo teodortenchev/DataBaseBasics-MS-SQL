0a800b4bb1653e6116098cee9d3d2162d01657e0
Teodor <teodortenchev@gmail.com>
2019-02-23 17:49:17 +0400

    Update 7. Programmability-and-Transactions-Solutionss.sql

--- 7. Programmability-and-Transactions-Solutionss.sql
+++ 7. Programmability-and-Transactions-Solutionss.sql
@@ -7,4 +7,24 @@ AS
 
 EXEC dbo.usp_GetEmployeesSalaryAbove35000
 
+GO
 --Problem 2
+CREATE PROC dbo.usp_GetEmployeesSalaryAboveNumber 
+       @SalaryLimit DECIMAL(18,4)
+AS
+  SELECT FirstName,LastName
+  FROM Employees
+  WHERE Salary >= @SalaryLimit
+
+EXEC dbo.usp_GetEmployeesSalaryAboveNumber 48100
+
+GO
+--Problem 3
+CREATE PROC usp_GetTownsStartingWith
+     @TownStartsWith nvarchar(10)
+AS
+   SELECT [Name] as Town
+   FROM Towns
+   WHERE [Name] LIKE @TownStartsWith + '%'
+
+EXEC usp_GetTownsStartingWith 'b'
