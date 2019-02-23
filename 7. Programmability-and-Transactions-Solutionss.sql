cbe0668d22b21c7e4f462a79606b6e0e02043f70
Teodor <teodortenchev@gmail.com>
2019-02-23 17:37:24 +0400

    Update 7. Programmability-and-Transactions-Solutionss.sql

--- 7. Programmability-and-Transactions-Solutionss.sql
+++ 7. Programmability-and-Transactions-Solutionss.sql
@@ -1,1 +1,10 @@
 --Problem 1
+CREATE PROCEDURE dbo.usp_GetEmployeesSalaryAbove35000
+AS
+  SELECT FirstName, LastName
+  FROM Employees
+  WHERE Salary > 35000
+
+EXEC dbo.usp_GetEmployeesSalaryAbove35000
+
+--Problem 2
