45631fc5799a5da3cea342dca29ec6f206f6ccd5
Teodor <teodortenchev@gmail.com>
2019-03-01 22:37:24 +0400

    Update 8. Trial Exam Solutions.sql

--- 8. Trial Exam Solutions.sql
+++ 8. Trial Exam Solutions.sql
@@ -229,4 +229,24 @@ AS
 CREATE PROC usp_CancelOrder
                   @OrderId INT, @CancelDate DATETIME
 AS
+  DECLARE @order INT = (SELECT Id From Orders WHERE Id = @OrderId)
+  
+  IF (@order IS NULL)
+    BEGIN
+      RAISERROR('The order does not exist!', 16, 1)
+      RETURN
+    END
+
+  DECLARE @orderDate DATETIME = (SELECT [DateTime] FROM Orders WHERE Id = @OrderId)
+  
+  IF (DATEDIFF(DAY, @orderDate, @CancelDate) > 3)
+    BEGIN
+      RAISERROR('You cannot cancel the order!', 16, 2)
+      RETURN
+    END
+
+  DELETE FROM OrderItems WHERE OrderId = @OrderId
+  DELETE FROM Orders WHERE Id = @OrderId
+
+
 
